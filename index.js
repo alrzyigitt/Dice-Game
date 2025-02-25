9768b2ba301034fe9ee9d10654e51e70b9ee8d37
alrzyigitt <ali335aga@gmail.com>
2025-02-25 15:52:10 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+if (performance.getEntriesByType("navigation")[0].type === "reload") {
+  diceRoll();
+}
+
+function diceRoll() {
+  var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+  var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+  document
+    .querySelector(".img1")
+    .setAttribute("src", "images/dice" + randomNumber1 + ".png");
+  document
+    .querySelector(".img2")
+    .setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Won!!";
+  }
+
+  if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Won!!";
+  } else if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw!!";
+  }
+}
